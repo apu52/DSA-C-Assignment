6a8fcd20d55f0832731a6a6434c584ae19548cf6
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-02-10 00:40:42 +0530

    Create Quick Sort.c

---
+++ Quick Sort.c
@@ -0,0 +1,55 @@
+/*  C program to implement Quick Sort using Arrays  */
+
+#include<stdio.h>
+
+void quicksort(int [],int,int);
+
+int main(){
+  int size,i;
+
+  printf("Enter size of the array: ");
+  scanf("%d",&size);
+
+  int x[size];
+
+  printf("Enter %d elements: ",size);
+  for(i=0;i<size;i++)
+    scanf("%d",&x[i]);
+
+  quicksort(x,0,size-1);
+
+  printf("Sorted elements: ");
+  for(i=0;i<size;i++)
+    printf(" %d",x[i]);
+
+  return 0;
+}
+
+void quicksort(int x[],int first,int last){
+    int pivot,j,temp,i;
+
+     if(first<last){
+         pivot=first;
+         i=first;
+         j=last;
+
+         while(i<j){
+             while(x[i]<=x[pivot]&&i<last)
+                 i++;
+             while(x[j]>x[pivot])
+                 j--;
+             if(i<j){
+                 temp=x[i];
+                  x[i]=x[j];
+                  x[j]=temp;
+             }
+         }
+
+         temp=x[pivot];
+         x[pivot]=x[j];
+         x[j]=temp;
+         quicksort(x,first,j-1);
+         quicksort(x,j+1,last);
+
+    }
+}
